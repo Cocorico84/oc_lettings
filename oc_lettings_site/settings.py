3192d7fc4488a9058efa868ff357052afe9c2ed8
Corentin <corentin@fanprime.io>
2022-01-05 09:45:49 +0100

    add ip in allowed_host

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -32,9 +32,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['oc-lettings-cz.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
